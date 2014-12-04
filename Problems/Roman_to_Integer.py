2fb1c5413d3a290be314bb29c4a7471ca9d8e325
Jaccorot <caozijun007@163.com>
2014-12-04 11:20:26 +0800

    修改Roman_to_Integer的计算问题

--- Problems/Roman_to_Integer.py
+++ Problems/Roman_to_Integer.py
@@ -5,50 +5,17 @@
 '''
 
 def romanToInt(s):
-        import re
-        if re.search('^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$',s)!= None:
-        	NumDic = {"pattern":"","retNum":0}
-        	RomanPattern = {
-        		"0":('','','','M'),
-        		"1":('CM','CD','D','C',100),
-        		"2":('XC','XL','L','X',10),
-        		"3":('IX','IV','V','I',1)
-        	}
-        	i = 3
-        	NumItems = sorted(RomanPattern.items())
-        	for RomanItem in NumItems:
-        		if RomanItem[0] != '0':
-        			patstr = NumDic["pattern"].join(['',RomanItem[1][0]])
-        			if re.search(patstr,s) != None:
-        				NumDic["retNum"] += 9*RomanItem[1][4]
-        				NumDic["pattern"] = patstr
-        			else:
-        				patstr = NumDic["pattern"].join(['',RomanItem[1][1]])
-        				if re.search(patstr,s) != None:
-        					NumDic["retNum"] += 4*RomanItem[1][4]
-        					NumDic["pattern"] = patstr
-        				else:
-        					patstr = NumDic["pattern"].join(['',RomanItem[1][2]])
-        					if re.search(patstr,s) != None:
-        						NumDic["retNum"] += 5*RomanItem[1][4]
-        						NumDic["pattern"] = patstr
-        		if NumDic["pattern"] == '':
-        			NumDic["pattern"] = '^'
-        		tempstr = ''
-        		sum = 0
-        		for k in range(0,4):
-        			pstr = RomanItem[1][3].join(['','{']).join(['',str(k)]).join(['','}'])
-        			patstr = NumDic["pattern"].join(['',pstr])
-        			if re.search(patstr,s) != None:
-        				sum = k*(10**i)
-        				tempstr = patstr
-        		if tempstr !='':
-        			NumDic["pattern"] = tempstr
-        		else:
-        			NumDic["pattern"] = patstr
-        		NumDic['retNum'] += sum
-        		i -= 1
-        	print type(NumDic['retNum'])
-        	return NumDic['retNum']
+    result = 0
+    romanDict = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    for i in s[-1::-1]:
+        symbol = 1
+        if i in ['I','X','C'] and 5 * romanDict[i] <= result:
+            symbol = -1
+        print 'symbol=',symbol,i,'---result=',result
 
-romanToInt('V')
+        result += romanDict[i] * symbol
+
+    print result
+
+
+romanToInt('MCDIV')
